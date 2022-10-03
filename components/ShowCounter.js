52842cd2ab474f4f39d824967f73e1e79ae7af74
James Ji <jamesx.ji@gmail.com>
2022-10-03 13:39:25 +1100

    todo, replace the image with a proper (no background) file from amy and then adjust the font properly for the countdown

--- components/ShowCounter.js
+++ components/ShowCounter.js
@@ -1,14 +1,14 @@
 const DateTimeDisplay = ({value, type}) => {
     return (
         <>
-            {value} {type}
+            <text class="font-bold xl:text-8xl">{value}</text> <text class="xl:text-4xl">{type} </text> 
         </>        
     )
 }
 
 const ShowCounter = ({ days, hours, minutes, seconds }) => {
     return (
-        <div>
+        <div className="bg-green-100">
             <DateTimeDisplay value={days} type={'DAYS'}/> <DateTimeDisplay value={hours} type={'HOURS'}/> <DateTimeDisplay value={minutes} type={'MINUTES'}/> <DateTimeDisplay value={seconds} type={'SECONDS'}/>
         </div>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,16 @@ module.exports = {
   theme: {
     fontFamily: {
       sans: ["Inter", "sans-serif"],
-    },
+    }
+    // fontSize: {
+    //   '1xl': '1rem',
+    //   '2xl': '1.5rem',
+    //   '3xl': '2rem',
+    //   '4xl': '4rem',
+    //   '5xl': '3rem',
+    //   '6xl': '6rem',
+    //   '7xl': '7rem',
+    // }
   },
   plugins: [
     // Typography
