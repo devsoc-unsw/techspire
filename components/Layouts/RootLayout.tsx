1a1cbb54abc8a3eea826c71dbc59cc48756d28c4
Michael Vo <zax@zaxu.xyz>
2022-10-09 18:58:55 +1100

    prevent horizontal overflow
    i have no fucking idea what's causing this so instead of solving it
    we're just working around it

--- components/Layouts/RootLayout.tsx
+++ components/Layouts/RootLayout.tsx
@@ -4,7 +4,7 @@ interface Props {}
 
 const RootLayout = ({ children }: PropsWithChildren<Props>) => {
   return (
-    <main className="selection:bg-accent h-screen snap-y snap-mandatory snap-always overflow-y-auto text-white">
+    <main className="selection:bg-accent h-screen snap-y snap-mandatory snap-always overflow-y-auto overflow-x-hidden text-white">
       {children}
     </main>
   );
