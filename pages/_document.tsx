7ca518ca87583a61874f59c7a02bf8a80b796531
Jared L <48422312+lhjt@users.noreply.github.com>
2022-10-09 23:22:32 +1100

    feat: add cloudflare analytics tag

---
+++ pages/_document.tsx
@@ -0,0 +1,37 @@
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+          <script
+            defer
+            src="https://static.cloudflareinsights.com/beacon.min.js"
+            data-cf-beacon='{"token": "226cda9f65b6445c91ebc75d0002d580"}'
+          />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
