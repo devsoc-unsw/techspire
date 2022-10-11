491524ed9606c5de2ae6954c1190fcd146637b5a
Michael Vo <zax@zaxu.xyz>
2022-10-11 16:04:41 +1100

    refactor: extract nav into component

--- components/Thingy.tsx
+++ components/Thingy.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 import Speaker from "./Speaker";
+import ThingyNav from "./ThingyNav";
 
 interface Props {
   speakers: {
@@ -15,29 +16,11 @@ interface Props {
 const Thingy = ({ speakers, focusedPage, setFocusedPage }: Props) => {
   return (
     <div className="flex gap-4 lg:px-4">
-      <nav
-        className="sticky top-0 hidden h-screen w-40 flex-col justify-center pl-2 transition-transform duration-[1500ms] md:flex lg:w-48"
-        style={{ transform: focusedPage ? "translateY(-100vh)" : undefined }}
-      >
-        {Object.keys(speakers).map((speaker, idx) => (
-          <div
-            key={speaker}
-            className={`group flex items-center opacity-80 transition-opacity hover:cursor-pointer hover:opacity-100 ${
-              focusedPage === idx + 1 &&
-              "animate-gradient-xy bg-gradient-to-br from-light to-[#f472b6] bg-clip-text text-transparent !opacity-100"
-            }`}
-            onClick={() => setFocusedPage(idx + 1)}
-          >
-            <div
-              className={`mx-2 h-px w-8 transition-colors group-hover:bg-white ${
-                focusedPage === idx + 1 &&
-                "animate-gradient-xy !bg-gradient-to-br from-light to-[#f472b6]"
-              }`}
-            />
-            {speaker}
-          </div>
-        ))}
-      </nav>
+      <ThingyNav
+        speakers={speakers}
+        focusedPage={focusedPage}
+        setFocusedPage={setFocusedPage}
+      />
       <main id="info-section" className={`flex-1`}>
         {Object.entries(speakers).map(
           ([speaker, { speakerName, text, video }], idx) => (
