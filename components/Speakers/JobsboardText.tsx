058f8d95f420f767e111cbf55f363797d4830f2a
Michael Vo <zax@zaxu.xyz>
2022-10-12 19:05:06 +1100

    feat: jobsboard text (#27)

--- components/Speakers/JobsboardText.tsx
+++ components/Speakers/JobsboardText.tsx
@@ -2,17 +2,16 @@ const JobsboardText = () => {
   return (
     <>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.
+        Founded in 2018 by Adam Tizzone, Jobsboard has played a crucial role in
+        kickstarting the careers of many CSESoc students. It has scraped by in
+        maintenance mode until this year, where a new team lead by Darian and
+        Joanna aimed to completely revamp the project.
       </p>
 
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.
+        With a modern UI, sponsor integrations, international support and more
+        to come, the future careers of CSESoc students are in bright hands. Come
+        listen to their journey and get a sneak peek before the release!
       </p>
     </>
   );
