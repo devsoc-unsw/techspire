d6e57c1688657bd08c243164b44f8ec1adc755f5
Michael Vo <zax@zaxu.xyz>
2022-10-12 20:31:51 +1100

    feat: update amazon video

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ const speakers = {
   Amazon: {
     speakerName: "Adam Leung",
     text: <AmazonText />,
-    video: "./videos/amazon.mp4",
+    video: "./videos/adam-leung.mp4",
   },
   Atlassian: {
     speakerName: "Ofir Zeevi",
