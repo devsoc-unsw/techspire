7c05d8300265c911d9375af67c2c017da2e1d011
Bob Chen <main.bobchen@gmail.com>
2024-10-15 16:17:32 -0700

    feat(2024): add ticket link

--- components/Speakers/Placeholder.tsx
+++ components/Speakers/Placeholder.tsx
@@ -4,6 +4,13 @@ const Placeholder = () => {
       <p style={{ width: "80vw" }}>
         Check back later for more updates to the list of speakers for this year.
       </p>
+      <p>Ticket sales begin Friday October 18 starting from $5</p>
+      <a
+        className="underline"
+        href="https://events.humanitix.com/techspire-2024"
+      >
+        Click here for ticketing information
+      </a>
     </>
   );
 };
